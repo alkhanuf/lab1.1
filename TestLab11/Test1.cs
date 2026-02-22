51d8c1c5f8591012a7f2b4bc64c833c2ac7d28fe
alkhanuf <simoon.phv@gmail.com>
2026-02-22 21:57:46 +0800

    написание тестов

---
+++ TestLab11/Test1.cs
@@ -0,0 +1,44 @@
+﻿using lab11;
+
+namespace TestLab11
+{
+    [TestClass]
+    public class LuckyNumberTests
+    {
+        [TestMethod]
+        public void IsLuckyNumber_123123()
+        {
+            bool res = LuckyNumber.IsLuckyNumber("123123");
+            Assert.IsTrue(res);
+        }
+
+        [TestMethod]
+        public void IsLuckyNumber_222234()
+        {
+            bool res = LuckyNumber.IsLuckyNumber("222234");
+            Assert.IsFalse(res);
+        }
+
+        [TestMethod]
+        public void IsLuckyNumber_111111()
+        {
+            bool res = LuckyNumber.IsLuckyNumber("111111");
+            Assert.IsTrue(res);
+        }
+
+        [TestMethod]
+        public void IsLuckyNumber_000000()
+        {
+            bool res = LuckyNumber.IsLuckyNumber("000000");
+            Assert.IsTrue(res);
+        }
+
+        [TestMethod]
+        public void IsLuckyNumber_444336()
+        {
+            bool res = LuckyNumber.IsLuckyNumber("444336");
+            Assert.IsTrue(res);
+        }
+
+    }
+}
