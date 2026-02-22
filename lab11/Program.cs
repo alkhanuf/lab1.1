1647023a53ff9a2bd8d13bef5af357ca0bf8fb7d
alkhanuf <simoon.phv@gmail.com>
2026-02-22 19:36:54 +0800

    определение счастливого числа

--- lab11/Program.cs
+++ lab11/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace lab11
+﻿//Определить, является ли заданное шестизначное число счастливым.
+//(Счастливым называют такое шестизначное число, что сумма его первых трех цифр равна сумме его последних трех цифр.)
+
+namespace lab11
 {
     internal class Program
     {
@@ -15,6 +18,31 @@
             }
 
             Console.WriteLine(number);
+
+            int sum1 = 0;
+            int sum2 = 0;
+
+            for (int i = 0; i < 3; i++) {
+                sum1 += int.Parse(number[i].ToString());
+            }
+
+            for (int i = 3; i < 6; i++)
+            {
+                sum2 += int.Parse(number[i].ToString());
+            }
+
+            Console.WriteLine(sum1);
+            Console.WriteLine(sum2);
+
+            if (sum1 == sum2)
+            {
+                Console.WriteLine("число " + number + " cчастливое");
+            }
+            else
+            {
+                Console.WriteLine("число " + number + " НЕ cчастливое");
+            }
+            
         }
     }
 }
